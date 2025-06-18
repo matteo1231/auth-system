0dc4581bff98a4807c509868a488cc4b80e26918
matteo1231 <smffronda@gmail.com>
2025-06-18 23:27:41 +0800

    correct case for model user import

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('./models/user.js');
+const User = require('./models/User.js');
 
 const app = express();
 app.use(express.json());
